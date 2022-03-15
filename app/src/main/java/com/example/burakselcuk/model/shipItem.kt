c3ee096a0836aec045a1c9bbb019de06dc8255f9
Burak <design.burakk@gmail.com>
2022-03-15 18:44:43 +0300

    data classes generated!

---
+++ app/src/main/java/com/example/burakselcuk/model/shipItem.kt
@@ -0,0 +1,10 @@
+package com.example.burakselcuk.model
+
+data class shipItem(
+    val capacity: Int,
+    val coordinateX: Int,
+    val coordinateY: Int,
+    val name: String,
+    val need: Int,
+    val stock: Int
+)
