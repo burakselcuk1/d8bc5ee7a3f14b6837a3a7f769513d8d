3e6ad5a8b4e27a242820717e9425897669a55bdc
Burak <design.burakk@gmail.com>
2022-03-15 19:31:56 +0300

    activity_main.xml and MainActivity.kt reviewed for bottom nav menu

--- app/src/main/java/com/example/burakselcuk/MainActivity.kt
+++ app/src/main/java/com/example/burakselcuk/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.burakselcuk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val navController = findNavController(R.id.fragmentContainerView)
+
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
