d2503e8ed661ece5b382aba4a3518c881920045c
Burak <design.burakk@gmail.com>
2022-03-15 19:31:31 +0300

    Application.kt created for hilt

--- app/src/main/java/com/example/burakselcuk/Application.kt
+++ app/src/main/java/com/example/burakselcuk/Application.kt
@@ -1,4 +1,8 @@
 package com.example.burakselcuk
 
-class Application {
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class Application:Application() {
 }
