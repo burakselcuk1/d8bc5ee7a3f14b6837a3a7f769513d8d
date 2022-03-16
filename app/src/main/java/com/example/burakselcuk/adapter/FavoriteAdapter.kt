e09e312f581ab7ba786b38cc9b8019a701fdbeb8
Burak <design.burakk@gmail.com>
2022-03-16 04:13:22 +0300

    FavoriteAdapter.kt created!

---
+++ app/src/main/java/com/example/burakselcuk/adapter/FavoriteAdapter.kt
@@ -0,0 +1,48 @@
+package com.example.burakselcuk.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.burakselcuk.R
+import com.example.burakselcuk.model.shipItem
+
+
+class FavoriteAdapter(private val dataSet: ArrayList<shipItem>) :
+    RecyclerView.Adapter<FavoriteAdapter.ViewHolder>() {
+
+    /**
+     * Provide a reference to the type of views that you are using
+     * (custom ViewHolder).
+     */
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView
+
+        init {
+            // Define click listener for the ViewHolder's View.
+            textView = view.findViewById(R.id.roomStationName)
+        }
+    }
+
+    // Create new views (invoked by the layout manager)
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        // Create a new view, which defines the UI of the list item
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.room_item, viewGroup, false)
+
+        return ViewHolder(view)
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+
+        // Get element from your dataset at this position and replace the
+        // contents of the view with that element
+        viewHolder.textView.text = dataSet[position].name
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    override fun getItemCount() = dataSet.size
+
+}
