301852b30bf4b0ca709a0819d89f253b9e23f981
Burak <design.burakk@gmail.com>
2022-03-16 03:31:34 +0300

    AppModule.kt reviewed

--- app/src/main/java/com/example/burakselcuk/di/AppModule.kt
+++ app/src/main/java/com/example/burakselcuk/di/AppModule.kt
@@ -1,10 +1,14 @@
 package com.example.burakselcuk.di
 
+import android.content.Context
+import androidx.room.Room
 import com.example.burakselcuk.Util.Cons.BASE_URL
+import com.example.burakselcuk.db.StationDB
 import com.example.burakselcuk.service.ShipApi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -28,5 +32,21 @@ object AppModule {
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
+
+    @Singleton
+    @Provides
+    fun provideStationDataBase(
+        @ApplicationContext context: Context
+    ) = Room.databaseBuilder(
+        context, StationDB::class.java,
+        "station_data"
+    ).build()
+
+    @Singleton
+    @Provides
+    fun provideStationDao(
+        db: StationDB
+    ) = db.FavoriteDao()
+
 
 }
