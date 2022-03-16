5ae9ace41f74c54cb2e737223d08237592809ca2
Burak <design.burakk@gmail.com>
2022-03-16 03:29:31 +0300

    shipItem.kt reviewed

--- app/src/main/java/com/example/burakselcuk/model/shipItem.kt
+++ app/src/main/java/com/example/burakselcuk/model/shipItem.kt
@@ -1,10 +1,16 @@
 package com.example.burakselcuk.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.io.Serializable
+
+@Entity(tableName = "station_data")
 data class shipItem(
+    @PrimaryKey(autoGenerate = true)
     val capacity: Int,
     val coordinateX: Int,
     val coordinateY: Int,
     val name: String,
     val need: Int,
     val stock: Int
-)
+):Serializable
