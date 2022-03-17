d048678eb83a127f0dbb71f3139be7d5b9360d52
Burak <design.burakk@gmail.com>
2022-03-18 02:33:38 +0300

    StationAdapter.kt reviewed reviewed

--- app/src/main/java/com/example/burakselcuk/adapter/StationAdapter.kt
+++ app/src/main/java/com/example/burakselcuk/adapter/StationAdapter.kt
@@ -23,27 +23,20 @@ class StationAdapter(private val dataSet: ship) :
         val stationStock: TextView
 
         init {
-            // Define click listener for the ViewHolder's View.
             stationName = view.findViewById(R.id.stationName)
             capacity = view.findViewById(R.id.capacity)
             stationStock = view.findViewById(R.id.stationStock)
         }
     }
 
-    // Create new views (invoked by the layout manager)
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
-        // Create a new view, which defines the UI of the list item
         val view = LayoutInflater.from(viewGroup.context)
             .inflate(R.layout.ship_item, viewGroup, false)
 
         return ViewHolder(view)
     }
 
-    // Replace the contents of a view (invoked by the layout manager)
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-
-        // Get element from your dataset at this position and replace the
-        // contents of the view with that element
         viewHolder.stationName.text = dataSet[position].name
         viewHolder.capacity.text = dataSet[position].capacity.toString()
         viewHolder.stationStock.text = dataSet[position].stock.toString()
@@ -59,8 +52,5 @@ class StationAdapter(private val dataSet: ship) :
             Log.e("Brk:StationAdapter",savedStation.name)
         }
     }
-
-    // Return the size of your dataset (invoked by the layout manager)
     override fun getItemCount() = dataSet.size
-
 }
