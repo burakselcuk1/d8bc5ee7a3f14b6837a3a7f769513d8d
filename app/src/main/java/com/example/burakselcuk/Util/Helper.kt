065c8a30dad67a11971b9b8ea81adcae4657ffc5
Burak <design.burakk@gmail.com>
2022-03-18 01:39:47 +0300

    Helper.kt reviewed

--- app/src/main/java/com/example/burakselcuk/Util/Helper.kt
+++ app/src/main/java/com/example/burakselcuk/Util/Helper.kt
@@ -7,15 +7,16 @@ class Helper {
     companion object {
         fun <ArrayList> getVersionsList(): ArrayList {
             var stationList = ArrayList<shipItem>()
-            stationList.add(shipItem(300, 3,7,"burak",5,1000))
-            stationList.add(shipItem(300, 3,7,"bozo",5,1000))
-            stationList.add(shipItem(300, 3,7,"burak",5,1000))
-            stationList.add(shipItem(300, 3,7,"burak",5,1000))
-            stationList.add(shipItem(300, 3,7,"burak",5,1000))
-
-
+            stationList.add(shipItem(300, 3,7,"DÜNYA",5,1000))
+            stationList.add(shipItem(300, 3,7,"TCRKL289",5,1000))
+            stationList.add(shipItem(300, 3,7,"MUKLSXCV",5,1000))
+            stationList.add(shipItem(300, 3,7,"BRNANDOK",5,1000))
+            stationList.add(shipItem(300, 3,7,"SKLMERCN",5,1000))
+            stationList.add(shipItem(300, 3,7,"ALPMRTKN",5,1000))
+            stationList.add(shipItem(300, 3,7,"MRTANDOP",5,1000))
+            stationList.add(shipItem(300, 3,7,"IBRKAL",5,1000))
+            stationList.add(shipItem(300, 3,7,"GKOLSKET",5,1000))
             return stationList as ArrayList
         }
     }
-
 }
