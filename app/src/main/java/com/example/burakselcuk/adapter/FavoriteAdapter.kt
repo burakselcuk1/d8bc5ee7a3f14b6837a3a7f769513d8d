42ea5ce59447841fd9e6334358b819fdd837e9d7
Burak <design.burakk@gmail.com>
2022-03-18 02:33:24 +0300

    FavoriteAdapter.kt reviewed

--- app/src/main/java/com/example/burakselcuk/adapter/FavoriteAdapter.kt
+++ app/src/main/java/com/example/burakselcuk/adapter/FavoriteAdapter.kt
@@ -12,37 +12,25 @@ import com.example.burakselcuk.model.shipItem
 class FavoriteAdapter(private val dataSet: ArrayList<shipItem>) :
     RecyclerView.Adapter<FavoriteAdapter.ViewHolder>() {
 
-    /**
-     * Provide a reference to the type of views that you are using
-     * (custom ViewHolder).
-     */
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val textView: TextView
 
         init {
-            // Define click listener for the ViewHolder's View.
             textView = view.findViewById(R.id.roomStationName)
         }
     }
 
-    // Create new views (invoked by the layout manager)
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
-        // Create a new view, which defines the UI of the list item
         val view = LayoutInflater.from(viewGroup.context)
             .inflate(R.layout.room_item, viewGroup, false)
 
         return ViewHolder(view)
     }
 
-    // Replace the contents of a view (invoked by the layout manager)
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-
-        // Get element from your dataset at this position and replace the
-        // contents of the view with that element
         viewHolder.textView.text = dataSet[position].name
     }
 
-    // Return the size of your dataset (invoked by the layout manager)
     override fun getItemCount() = dataSet.size
 
 }
